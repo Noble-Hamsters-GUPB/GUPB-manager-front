8d0802ca7deafeb95240c8fe24e754c993f6d3ea
Krzysztof Retkiewicz <305406@student.agh.edu.pl>
2021-03-31 23:01:02 +0200

    tournament form done

---
+++ src/components/tournament-list/index.tsx
@@ -0,0 +1,15 @@
+import {FC} from "react";
+import styles from "./styles.module.css"
+
+export const TournamentForm: FC = (props) => {
+
+
+    return (
+        <div>
+            <p> FORM TBD </p>
+            <form>
+                <input></input>
+            </form>
+        </div>
+    )
+}

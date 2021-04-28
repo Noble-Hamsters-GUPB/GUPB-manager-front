f9bff8f6d447e759c3c407482c10052b4063375c
Krzysztof Retkiewicz <305406@student.agh.edu.pl>
2021-04-28 13:37:39 +0200

    Round progress component done

--- src/Components/tournament-progression/index.tsx
+++ src/Components/tournament-progression/index.tsx
@@ -1,16 +1,58 @@
-import {FC} from "react";
+import {FC, useState, useEffect} from "react";
 import styles from "./styles.module.css"
-import {Box, CircularProgress, LinearProgress, Paper, Typography} from "@material-ui/core";
+import {Box, CircularProgress, createStyles, LinearProgress, makeStyles, Paper, Typography, Theme} from "@material-ui/core";
+import {green, orange, red, yellow} from "@material-ui/core/colors";
 
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        secondsProgress: {
+            color: red[500],
+        },
+        minutesProgress: {
+            color: orange[500]
+        },
+        hoursProgress: {
+            color: yellow[500]
+        },
+    })
+)
 export const TournamentProgression: FC = (props) => {
+    const classes = useStyles()
 
+    const [round, setRound] = useState(9) //TODO: Zapiąć z gotowym turniejem jak będzie
+
+    const [maxRounds, setMaxRounds] = useState(14) //TODO: Zapiąć z gotowym turniejem jak będzie
+
+    const [time, setTime] = useState(90) //TODO: Zapiąć z gotowym turniejem jak będzie
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime((prevTime) => (prevTime >= 0 ? prevTime - 1 : 0))
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+
+    const formatTime = (time:number) =>{
+        if(time>24*3600){
+            return Math.floor(time/(24*3600)) + "d " + Math.round(time%(24*3600)/(3600)) + "h"
+        }else if(time>3600){
+            return Math.floor(time/3600) + "h " + Math.round(time%3600/(60)) + "min"
+        }else if(time>60){
+            return Math.floor(time/(60)) + "min " + time%60 + "sec"
+        }else if(time>0){
+            return time + "sec"
+        }else{
+            return "Running..."
+        }
+    }
 
     return <Paper>
         <Box
             padding={"1em"}
             width={"8em"}
         >
-            this is tournament progression
             <Box
                 className={styles.roundProgress}
                 paddingBottom={"1em"}
@@ -20,12 +62,24 @@ export const TournamentProgression: FC = (props) => {
             >
 
                 <Box>
-                    <Typography variant={"caption"}>Round progress: 3/4</Typography>
-                    <LinearProgress variant={"determinate"} color={"primary"} value={75}/>
+                    <Typography variant={"caption"}>Round progress: <b>{round}/{maxRounds}</b></Typography>
+                    <LinearProgress variant={"determinate"} color={"primary"} value={round/maxRounds*100}/>
                 </Box>
             </Box>
             <Box position={"relative"} display={"inline-flex"}>
-                <CircularProgress variant={"determinate"} size={"8em"} thickness={2} value={100}/>
+                <Box position={"absolute"}>
+                    <CircularProgress className={classes.secondsProgress} variant={time > 0 ? "determinate" : "indeterminate"} size={"8em"} thickness={2}
+                                      value={Math.min(Math.max(time/0.6, 0), 100)}/>
+                </Box>
+                <Box position={"absolute"} visibility={time <= 0? "hidden" : "visible"}>
+                    <CircularProgress className={classes.minutesProgress} variant={"determinate"} size={"8em"} thickness={2}
+                                      value={Math.min(Math.max((time-60)/36, 0), 100)}/>
+                </Box>
+                <Box visibility={time <= 0? "hidden" : "visible"}>
+                    <CircularProgress className={classes.hoursProgress} variant={"determinate"} size={"8em"} thickness={2}
+                                      value={Math.min(Math.max((time-3600)/(24 * 36), 0), 100)}/>
+                </Box>
+
                 <Box
                     position={"absolute"}
                     display={"flex"}
@@ -36,7 +90,7 @@ export const TournamentProgression: FC = (props) => {
                     bottom={0}
                     right={0}
                 >
-                    {"12h 43min"}
+                    {formatTime(time)}
                 </Box>
             </Box>
 

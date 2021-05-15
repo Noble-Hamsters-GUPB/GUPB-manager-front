48b0fde364fe790ea543911e9fd9232b34210a95
Iffern <kicajka1999@gmail.com>
2021-05-15 20:48:49 +0200

    [NH-17] Updated prompted message

--- src/Components/library-list-remove-dialog/index.tsx
+++ src/Components/library-list-remove-dialog/index.tsx
@@ -1,4 +1,4 @@
-import {Button, Dialog, DialogActions, DialogTitle} from "@material-ui/core";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle} from "@material-ui/core";
 import {Link} from "react-router-dom";
 import RequirementService from "../../services/RequirementService";
 
@@ -13,6 +13,10 @@ export const LibraryRemoveDialog= (props: {library, libList, setLibList, url}) =
     return(
         <Dialog open={true} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
             <DialogTitle>Are you sure you want to remove {props.library.name} library?</DialogTitle>
+            <DialogContent>
+                <div>If you will remove this library, it will be no longer available for participants. Lack of that
+                library may also cause some bots compatibility issues</div>
+            </DialogContent>
             <DialogActions>
             <Button component={Link} to={props.url}
                 variant="contained" color="primary" autoFocus>

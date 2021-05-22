69ac1d446eaeaa1394842953388e87a0a6475267
Tequilac <58514571+tequilac@users.noreply.github.com>
2021-05-22 17:40:36 +0200

    Update tournament form

--- src/Components/tournament-form/index.tsx
+++ src/Components/tournament-form/index.tsx
@@ -19,20 +19,20 @@ import TournamentService from '../../services/TournamentService';
 
 export const TournamentForm: FC = (props) => {
 
-    const [date, setDate] = useState(getCurrentDate())
     const [name, setName] = useState("")
     const [accessMode, setAccessMode] = useState("OPEN")
+    const [invitationCode, setInvitationCode] = useState("")
 
-    const [dateError, setDateError] = useState(false);
     const [nameError, setNameError] = useState(false)
-
-    useEffect(() => {
-        setDateError(false)
-    },[date])
+    const [invitationCodeError, setInvitationCodeError] = useState(false)
 
     useEffect(() => {
         setNameError(false)
     },[name])
+
+    useEffect(() => {
+        setInvitationCodeError(false)
+    },[invitationCode])
 
     const submitTournament = (e) => {
         let errorFlag = false;
@@ -42,8 +42,8 @@ export const TournamentForm: FC = (props) => {
             errorFlag = true;
         }
 
-        if(new Date(date) < new Date()){
-            setDateError(true);
+        if(invitationCode === "" && accessMode === "INVITE_ONLY"){
+            setInvitationCodeError(true);
             errorFlag = true;
         }
 
@@ -53,7 +53,8 @@ export const TournamentForm: FC = (props) => {
         }
 
 
-        TournamentService.createTournament({"date": date, "name": name, "accessMode": accessMode})
+        TournamentService.createTournament({"name": name, "accessMode": accessMode,
+            "creator": 1, "invitationCode": invitationCode})
     }
 
     return (
@@ -61,40 +62,30 @@ export const TournamentForm: FC = (props) => {
             <IconButton component={Link} to={'/tournaments'} className={styles.closeButton}><CloseIcon/></IconButton>
             <DialogTitle className={styles.formTitle}>Create Tournament</DialogTitle>
             <DialogContent className={styles.formDialogContent}>
-                <TextField error={nameError} fullWidth label={nameError?"Tournament name cannot be empty":"Tournament name"} onChange={(e) => setName(e.target.value)}/>
-                <TextField error={dateError} fullWidth defaultValue={date} label={dateError?"Provide a future date":"Start time"} type="datetime-local"
-                           onChange={(e) => setDate(e.target.value)}/>
+                <TextField error={nameError} fullWidth label={nameError?"Tournament name cannot be empty":"Tournament name"}
+                           onChange={(e) => setName(e.target.value)}/>
                 <FormControl className={styles.form} component={"fieldset"}>
                     <FormLabel component="legend">Access mode</FormLabel>
                     <RadioGroup className={styles.formRadios} row aria-label="Access" value={accessMode}
                                 onChange={(e) => setAccessMode(e.target.value)}>
                         <FormControlLabel value="OPEN" control={<Radio/>} label="Open"/>
-                        <FormControlLabel value="RESTRICTED" control={<Radio/>} label="Restricted"/>
                         <FormControlLabel value="INVITE_ONLY" control={<Radio/>} label="Invite only"/>
                     </RadioGroup>
                 </FormControl>
+                {accessMode === "INVITE_ONLY"?
+                    <TextField error={invitationCodeError} fullWidth label={invitationCodeError?"Invitation code cannot be empty":"Invitation code"}
+                                                         onChange={(e) => setInvitationCode(e.target.value)}/> : null}
                 <DialogActions className={styles.submitAction}>
-                    <Link to={(dateError || nameError)?"#":"/tournaments"}  style={{ textDecoration: 'none' }}>
+                    <Link to={(invitationCodeError || nameError)?"#":"/tournaments"}  style={{ textDecoration: 'none' }}>
                         <Button
                             variant="contained"
                             color="secondary"
                             onClick={(e) => submitTournament(e)}
-                            disabled={dateError || nameError}
+                            disabled={invitationCodeError || nameError}
                         >CREATE</Button>
                     </Link>
                 </DialogActions>
             </DialogContent>
         </Dialog>
     )
-
-    function getCurrentDate(separator='-'){
-
-        let newDate = new Date()
-        let date = newDate.getDate();
-        let month = newDate.getMonth() + 1;
-        let year = newDate.getFullYear();
-        let time = newDate.getHours()+":"+newDate.getMinutes();
-
-        return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}T${time}`
-    }
 }

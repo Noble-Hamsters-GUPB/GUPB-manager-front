588ae16cc6bf491c32f380ed8dac009e76d4579a
Tequilac <58514571+tequilac@users.noreply.github.com>
2021-05-29 20:26:39 +0200

    Fix library request form

--- src/Components/library-request-dialog/index.tsx
+++ src/Components/library-request-dialog/index.tsx
@@ -18,19 +18,12 @@ import RequirementService from "../../services/RequirementService";
 
 export const LibraryRequestForm = (props:{libraries, addLibrary}) => {
     const [name, setName] = useState("")
-    const [version, setVersion] = useState("")
-    const [open, setOpen] = useState(true)
 
     const [nameError, setNameError] = useState(false)
-    const [versionError, setVersionError] = useState(false)
 
     useEffect(() => {
         setNameError(false)
     },[name])
-
-    useEffect(() => {
-        setVersionError(false)
-    },[version])
 
     const submitLibrary= (e) => {
         let errorFlag = false;
@@ -40,18 +33,12 @@ export const LibraryRequestForm = (props:{libraries, addLibrary}) => {
             errorFlag = true;
         }
 
-
-        if(version===""){
-            setVersionError(true);
-            errorFlag = true;
-        }
-
         if(errorFlag){
             e.preventDefault();
             return;
         }
 
-        let newLibrary = {packageInfo: name+"=="+version, status: "PENDING", teamId: 1, tournamentId: 1}
+        let newLibrary = {packageInfo: name, status: "PENDING", teamId: 1, tournamentId: 1}
 
         //props.addLibrary([...props.libraries, newLibrary])
         //TODO: create or update round (backend)
@@ -63,17 +50,15 @@ export const LibraryRequestForm = (props:{libraries, addLibrary}) => {
             <IconButton component={Link} to='/tournament-participant' className={styles.closeButton}><CloseIcon/></IconButton>
             <DialogTitle className={styles.formTitle}>Request Library</DialogTitle>
             <DialogContent className={styles.formDialogContent}>
-                <TextField error={nameError} fullWidth label={nameError?"Library name cannot be empty":"Library name"}
+                <TextField error={nameError} fullWidth label={nameError?"Library cannot be empty":"Library"}
                            onChange={(e) => setName(e.target.value)}/>
-                <TextField error={versionError} fullWidth  label={versionError?"Version cannot be empty":"Version"}
-                           onChange={(e) => setVersion(e.target.value)}/>
                 <DialogActions className={styles.submitAction}>
-                    <Link Link to={(versionError || nameError)?"#":"/tournament-participant"}  style={{ textDecoration: 'none' }}>
+                    <Link Link to={(nameError)?"#":"/tournament-participant"}  style={{ textDecoration: 'none' }}>
                         <Button
                             variant="contained"
                             color="secondary"
                             onClick={(e) => submitLibrary(e)}
-                            disabled={versionError || nameError}
+                            disabled={nameError}
                         >REQUEST</Button>
                     </Link>
                 </DialogActions>
